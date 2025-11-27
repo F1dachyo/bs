1f21df9552c9056d907fd9c00bc8fe7dfdf2a0a4
Feedachyo <sanyastik@bk.ru>
2025-11-27 13:29:37 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Раздаём статичные файлы (картинки, css, js)
+app.mount("/static", StaticFiles(directory="."), name="static")
+
+@app.get("/")
+async def root():
+    return FileResponse("index.html")
